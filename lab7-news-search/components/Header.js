60c3898570da6042cc94b473dacebafaddd4fc34
x00125134a <43752088+x00125134a@users.noreply.github.com>
2018-12-12 13:01:50 +0000

    Add files via upload

---
+++ lab7-news-search/components/Header.js
@@ -0,0 +1,18 @@
+const Header = () => (
+    <div>
+        <h2 className="title">Around the world news</h2>
+        <style jsx>{`
+
+
+            h2.title {
+                font-family: "Arial";
+                color: #535151;
+                padding: 5px;
+            }
+
+
+        `}</style>
+    </div> 
+ )
+ 
+ export default Header;
---
+++ lab7-news-search/components/Nav.js
@@ -0,0 +1,73 @@
+// Site Navigation menu
+// https://www.sitepoint.com/responsive-fluid-width-variable-item-navigation-css/
+// https://www.w3schools.com/Css/css_navbar.asp
+
+import Link from 'next/link';
+
+const Nav = () => (
+   <div>
+       <nav>
+           <ul>
+            <li><Link href="/index"><a>Ireland</a></Link></li>
+            <li><Link href="/usa"><a>America</a></Link></li>
+            <li><Link href="/australia"><a>Australia</a></Link></li>
+            <li><Link href="/uk"><a>UK </a></Link></li>
+           </ul>
+       </nav>
+       {/* Define css for this page or component */}
+       {/* Note back ticks `` surrounding css are required */}
+       <style jsx>{`
+        nav {
+            max-width: 900px;
+            background: #f0f0f0;
+            border: 1px solid #ccc;
+            border-right: none;            
+        }
+
+
+        nav ul {
+            d list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        nav ul li {
+            display: inline;
+            list-style: none;
+            float: left;
+            flex-grow: 1;
+            text-align: center;
+            border-left: 1px solid #fff;
+            border-right: 1px solid #ccc;
+            width: 16.6667%; /* fallback for non-calc() browsers */
+            width: calc(100% / 6);
+            box-sizing: border-box;
+        }
+
+        nav ul li:first-child {
+            border-left: none;
+        }
+
+        nav ul li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;;
+        }
+
+        nav ul li:hover {
+            background: black;
+        }
+        nav ul li a:hover {
+            color: white;
+        }
+
+        `}</style>
+   </div> 
+)
+
+export default Nav;
+
